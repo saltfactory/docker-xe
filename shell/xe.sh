a128bab297b6f5d7c9a90f5c56d47d0360e7a6f4
saltfactory <saltfactory@gmail.com>
2015-07-03 00:15:12 +0900

    config.sh 설정파일 분리, 라이센스추가

--- shell/xe.sh
+++ shell/xe.sh
@@ -1,24 +1,6 @@
 #!/bin/bash
 
-############################################################
-## docker project name
-PROJECT_NAME="docker-xe"
-############################################################
-## docker configurations
-DOCKER_USER="saltfactory"
-DOCKER_VERSION="1.8.3"
-DOCKER_NAME="xe"
-DOCKER_IMAGE="$DOCKER_USER/$DOCKER_NAME:$DOCKER_VERSION"
-############################################################
-## container configurations
-DOCKER_CONTAINER_NAME="demo-xe"
-DOCKER_CONTAINER_PORT="7000"
-############################################################
-## container volumes
-XE_HOME="/Users/saltfactory/shared/xe-core"
-XE_FILES="/Users/saltfactory/shared/xe-core/files"
-XE_LOGS="/Users/saltfactory/shared/logs"
-############################################################
+source ./shell/config.sh
 
 function is_running {
   docker ps | grep $DOCKER_CONTAINER_NAME
